009895fa6f416d2e4cd72980418c052ac0bac779
Damir Bukvasevic <damirbukvasevic@gmail.com>
2024-09-05 18:41:51 +0200

    dodavanje app.

--- Program.cs
+++ Program.cs
@@ -17,24 +17,40 @@ builder.Services.AddDbContext<VodjenjeKaficaContext>(
         opcije.UseSqlServer(builder.Configuration.GetConnectionString("VodjenjeKaficaContext"));
     }
     );
+builder.Services.AddCors(opcije =>
+{
+    opcije.AddPolicy("CorsPolicy",
+        builder =>
+            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+    );
+
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI(opcije =>
     {
         opcije.ConfigObject.AdditionalItems.Add("requestSnippetsEnabled", true);
         opcije.EnableTryItOutByDefault();
     });
-}
+//}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseStaticFiles();
+
+app.UseDefaultFiles();
+
+app.MapFallbackToFile("index.html");
+
+app.UseCors("CorsPolicy");
 
 app.Run();
