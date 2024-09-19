f94dcc4462f3e769782ea0e82db3ce5fa1b63278
Damir Bukvasevic <damirbukvasevic@gmail.com>
2024-09-19 18:58:36 +0200

    dodavanje footera

--- FrontendKafic/src/components/NavBarKafic.jsx
+++ FrontendKafic/src/components/NavBarKafic.jsx
@@ -12,7 +12,7 @@ export default function NavBarKafic(){
     return(
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Kafic APP</Navbar.Brand>
+        <Navbar.Brand href="/">Kafic APP</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
